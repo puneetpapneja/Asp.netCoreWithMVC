3d60880a3a00596edb5b63e11d3491c33272e839
puneet.papneja <puneet.papneja@metacube.com>
2021-02-02 15:58:18 +0530

    Configure EntityFramework

---
+++ BookStore/BookStore/Models/BookDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Models
+{
+    public class BookDBContext:DbContext
+    {
+        public BookDBContext(DbContextOptions<BookDBContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<BookModel> bookModel  { get; set; }
+    }
+}
--- BookStore/BookStore/StartUp.cs
+++ BookStore/BookStore/StartUp.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models;
 
 namespace BookStore
 {
@@ -13,6 +16,8 @@ namespace BookStore
     {
         public void ConfigureServices(IServiceCollection service)
         {
+            service.AddDbContext<BookDBContext>(options => 
+                 options.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=bookstore;integrated security=true"));            
             service.AddMvc();
 #if DEBUG
             service.AddRazorPages().AddRazorRuntimeCompilation();
